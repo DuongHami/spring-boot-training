684550301f92caeef96bee1ea4cceb309ed3073a
HamiDuong <146204133+DuongHami@users.noreply.github.com>
2024-03-22 13:13:29 +0100

    Create GlobalExceptionHandlerTest.java

---
+++ src/test/java/com/example/springboottraining/exception/GlobalExceptionHandlerTest.java
@@ -0,0 +1,31 @@
+package com.example.springboottraining.exception;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GlobalExceptionHandlerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void testHandleToDoNotFoundException() throws Exception {
+        mvc.perform(get("/todo/{id}", 100L))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$", is("Die Entit�t konnte nicht gefunden werden")));
+    }
+}
