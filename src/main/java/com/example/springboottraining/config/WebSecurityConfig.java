093d3b1e6f44b85fe9b53e52c25864a0b26ea571
HamiDuong <146204133+DuongHami@users.noreply.github.com>
2024-03-25 11:52:40 +0100

    added PasswordEncoder

--- src/main/java/com/example/springboottraining/config/WebSecurityConfig.java
+++ src/main/java/com/example/springboottraining/config/WebSecurityConfig.java
@@ -1,29 +1,59 @@
 package com.example.springboottraining.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.io.IOException;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+//                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authentication) -> authentication
+                        .requestMatchers("/", "index.html").permitAll()
+                        .requestMatchers("helloWorld.html").hasRole("HELLO_WORLD")
                         .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
+                ).httpBasic(withDefaults());;
         return http.build();
     }
 
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-//        manager.createUser(User.withDefaultPasswordEncoder().username("123").password("123").roles("USER").build());
-//        return manager;
-//    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails helloWorld = User.builder()
+                .username("hw")
+                .password(passwordEncoder.encode("hw"))
+                .roles("HELLO_WORLD")
+                .build();
+
+        return new InMemoryUserDetailsManager(user,helloWorld);
+    }
 }
